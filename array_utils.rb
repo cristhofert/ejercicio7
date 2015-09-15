dc50aafc482f437710452c96ebf51642afdc4734
Cristhofer Travieso <cristhofert97@gmail.com>
2015-09-15 18:59:07 -0300

    accedio al primer elemento y operador ternario

--- array_utils.rb
+++ array_utils.rb
@@ -1,9 +1,9 @@
 module ArrayUtils
 	def self.array_max(param_array)
-		max = param_array
+		max = param_array[0]
 
 		param_array.each do |element|
-			max = element if element > max
+			max = (element > max) ? element
 		end
 
 		max 
